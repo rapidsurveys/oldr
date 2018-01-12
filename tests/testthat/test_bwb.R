d125347a09c1f3984e173ea4d6bebb8c9890cbb8
ernestguevarra <ernestgmd@gmail.com>
2018-01-12 07:09:05 +0000

    add numeric test for boot

--- tests/testthat/test_bwb.R
+++ tests/testthat/test_bwb.R
@@ -1,12 +1,17 @@
 library(ramOP)
 context("Blocked weighted bootstrap")
 
+boot <- bootBW(x = indicators.ALL,
+               w = testPSU,
+               statistic = bootClassic,
+               params = "ADL01",
+               outputColumns = "ADL01",
+               replicates = 9)
+
 test_that("boot is a data frame", {
-  boot <- bootBW(x = indicators.ALL,
-                 w = testPSU,
-                 statistic = bootClassic,
-                 params = "ADL01",
-                 outputColumns = "ADL01",
-                 replicates = 9)
   expect_is(boot, "data.frame")
+})
+
+test_that("boot vectors are numeric", {
+  expect_is(boot[1], "numeric")
 })
