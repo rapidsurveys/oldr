b6fed2c7f082a56df6f2e022eaf5c7a4482c68e4
ernestguevarra <ernestgmd@gmail.com>
2018-01-30 03:03:41 +0000

    update tests for charts

--- tests/testthat/test_charts.R
+++ tests/testthat/test_charts.R
@@ -22,3 +22,102 @@ test_that("output chart is present", {
 test_that("is integer", {
   expect_is(test, "integer")
 })
+
+test <- chartMF(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.MF.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartDDS(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.DDS.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartK6(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.K6.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartADL(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.ADL.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartWASH(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.WASH.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartCSID(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.CSID.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartWG(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.WG.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartHHS(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.HHS.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
+
+test <- chartIncome(x = indicators.ALL, filename = "TEST")
+test
+
+test_that("output chart is present", {
+  expect_true(file.exists("TEST.Incomes.png"))
+})
+
+test_that("is integer", {
+  expect_is(test, "integer")
+})
