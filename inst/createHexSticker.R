a034f23de772d38b62a504cdc6cc50fe801277fe
Ernest Guevarra <ernest@validmeasures.org>
2019-04-11 21:53:58 +0100

    update sticker

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -3,5 +3,5 @@ library(hexSticker)
 sticker("inst/figures/elderly.png",
         s_x = 1, s_y = 0.8, s_width = 0.8,
         package = "oldr", p_color = "#E3BB00", p_y = 1.5, p_size = 12,
-        h_size = 1, h_color = "#E3BB00", h_fill = NA,
+        h_size = 1.5, h_color = "#E3BB00", h_fill = NA,
         filename = "man/figures/oldr.png")
