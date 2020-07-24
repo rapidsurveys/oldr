80cc9aea75dbc5448b25ca20a9fb89993f4762b0
Ernest Guevarra <ernest@guevarra.io>
2020-07-24 09:07:51 +0100

    remove create_op_pdf tests

--- tests/testthat/test-10-report.R
+++ tests/testthat/test-10-report.R
@@ -6,7 +6,6 @@ test_that("output file exists", {
   expect_true(file.exists(paste(tempdir(), "TEST.report.csv", sep = "/")))
 })
 
-
 report_op_html(estimates = estimate_op_all(x = indicators.ALL,
                                            w = testPSU,
                                            replicates = 19),
@@ -18,14 +17,3 @@ test_that("output file exists", {
   expect_true(file.exists(paste(tempdir(), "htmlReport.Rmd", sep = "/")))
 })
 
-
-report_op_pdf(estimates = estimate_op_all(x = indicators.ALL,
-                                          w = testPSU,
-                                          replicates = 19),
-              svy = testSVY,
-              filename = paste(tempdir(), "pdfReport", sep = "/"))
-
-test_that("output file exists", {
-  expect_true(file.exists(paste(tempdir(), "pdfReport.pdf", sep = "/")))
-  expect_true(file.exists(paste(tempdir(), "pdfReport.Rmd", sep = "/")))
-})
